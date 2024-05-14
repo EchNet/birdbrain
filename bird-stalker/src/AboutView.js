f8dbc379bf126f04ec0d6f51454433ac5e1c5c74
James Echmalian <ech@ech.net>
2024-05-14 13:46:26 -0400

    About

--- bird-stalker/src/AboutView.js
+++ bird-stalker/src/AboutView.js
@@ -2,7 +2,74 @@ import React from 'react';
 
 function AboutView() {
   return (
-    <div>Coming soon</div>
+    <>
+      <section>
+        <h3>Hello, Birders!</h3>
+        <p>
+          <span className="logo">Bird Stalker</span> is a companion to
+          the <a href="https://ebird.org" target="ebird">eBird website/app.</a>
+        </p>
+        <p>
+          <ul>
+            <li>
+              Are you traveling and want to find out what interesting birds can be seen in the area?  
+            </li>
+            <li>
+              Are you stalking a particular, elusive species and want to know where it has been seen lately?
+              <br/>
+            </li>
+            <li>
+              Are you just looking to add variety to your birding adventure?
+            </li>
+          </ul>
+        </p>
+        <p>
+          eBird can answer your questions, but it takes a lot of clicking and typing!  (Or, poking and 
+          swiping, depending on your tech preferences!)
+        </p>
+        <p>
+          <span className="logo">Bird Stalker</span> aims to take the fuss out of our common obsession.
+        </p>
+      </section>
+      <section>
+        <h3>Feedback</h3>
+        <p>
+          Having trouble getting Bird Stalker to work right?
+        </p>
+        <p>
+          Got an idea for improving Bird Stalker?
+        </p>
+        <p>
+          Email us: <a href="mailto:birdstalker@ech.net" target="email">birdstalker@ech.net</a>
+        </p>
+      </section>
+      <section>
+        <h3>About Me</h3>
+        <p>
+          <a href="https://ebird.org/profile/NTIwNjcy" target="ebird">My eBird profile.</a>
+        </p>
+        <p>
+          I like nature and getting outdoors. I like building software to simplify everyday complications.
+        </p>
+        <p>
+          My passion for birding began at an early age, when the family bookcase held such classic
+          Golden Guide Nature Pocket Guides as "Trees", "Stars", and "Rocks and Minerals", though none attracted
+          the attention of this young reader as did "Birds".
+        </p>
+      </section>
+      <section>
+        <h3>Support Bird Stalker!</h3>
+        <p>
+          <a href="https://paypal.me/echnet" target="paypal">
+            <div>
+              <img src="/img/coffee.png" height="105"/>
+              <br/>
+              Buy me a coffee.
+            </div>
+          </a>
+        </p>
+      </section>
+    </>
   );
 }
 
--- bird-stalker/src/Header.js
+++ bird-stalker/src/Header.js
@@ -6,21 +6,26 @@ import AppContext from './AppContext';
 import './HamburgerButton.css';
 
 
-function ActionMenu(props) {
-  const { apiKey, location } = useContext(AppContext);
+function ActionMenu({ minimized }) {
+  const { apiKey, location, setMenuOpen } = useContext(AppContext);
   const navigate = useNavigate();
 
   const birdListDisabled = !apiKey || !location;
   const locationPrompt = location ? "Start stalking somewhere else" : "Choose a location to stalk";
   const apiKeyPrompt = apiKey ? "Update API Key" : "Connect to ebird.org API";
 
+  function goto(path) {
+    setMenuOpen(false);
+    navigate(path);
+  }
+
   return (
-    <div className="action-menu">
+    <div className={`action-menu${minimized ? " minimized" : ""}`}>
       <Button variant="outlined" disabled={birdListDisabled}
           onClick={() => navigate("/birds")}>See the list of local birds</Button>
-      <Button variant="outlined" onClick={() => navigate("/location")}>{locationPrompt}</Button>
-      <Button variant="outlined" onClick={() => navigate("/apikey")}>Update API Key</Button>
-      <Button variant="outlined" onClick={() => navigate("/about")}>About Bird Stalker</Button>
+      <Button variant="outlined" onClick={() => goto("/location")}>{locationPrompt}</Button>
+      <Button variant="outlined" onClick={() => goto("/apikey")}>{apiKeyPrompt}</Button>
+      <Button variant="outlined" onClick={() => goto("/about")}>About Bird Stalker</Button>
     </div>
   )
 }
@@ -36,7 +41,7 @@ function Header() {
   return (
     <>
       <div className="header-strip">
-        <img src="/img/striplogo.png" height="48" alt="Bird Stalker"/>
+        <img className="striplogo" src="/img/striplogo.png" alt="Bird Stalker"/>
 
         <div className={`hamburger-button${ menuOpen ? " close" : ""}`} onClick={handleHamburgerClick}>
           <div className="btn-line" />
@@ -44,7 +49,7 @@ function Header() {
           <div className="btn-line" />
         </div>
       </div>
-      { menuOpen ? <ActionMenu/> : null }
+      <ActionMenu minimized={!menuOpen}/>
     </>
   );
 }
